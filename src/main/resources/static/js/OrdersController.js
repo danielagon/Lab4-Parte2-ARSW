82f7fd82548383f253055aaec4809e2915cad66e
danielagon <daniela.gonzalezramirez@hotmail.com>
2018-09-25 10:26:44 -0500

    Select table - CRUD

--- src/main/resources/static/js/OrdersController.js
+++ src/main/resources/static/js/OrdersController.js
@@ -1,78 +1,125 @@
 var OrdersControllerModule = (function () {
-
-  var showOrdersByTable = function () {
     
-    var callback = {
-        
-        onSuccess: function(orders){
-            
-            for (j in orders){
+    var orders;
+
+    var showOrdersByTable = function () {
     
-                var dvTable = document.getElementById("Tables");
+        var callback = {
 
-                var header = new Array();
-                    header.push("Product");
-                    header.push("Quantity");
+            onSuccess: function(orders){
 
-                var table = document.createElement("table");
-                table.border = "1";
-                table.setAttribute("id","Table"+j);
-                table.setAttribute("class","table table-sm table-dark");
+                for (j in orders){
 
-                var column = 2;
-                var row = table.insertRow(-1);
-                var headerTable = document.createElement("th");
-                headerTable.setAttribute("colspan","3");
-                headerTable.innerHTML = "Table "+j;
-                row.appendChild(headerTable);
+                    var dvTable = document.getElementById("Tables");
 
-                var row = table.insertRow(-1);
-                for (var i=0;i<column;i++){
-                    var headerCell = document.createElement("th");
-                    headerCell.innerHTML = header[i];
-                    row.appendChild(headerCell);
+                    var header = new Array();
+                        header.push("Product");
+                        header.push("Quantity");
+
+                    var table = document.createElement("table");
+                    table.border = "1";
+                    table.setAttribute("id","Table"+j);
+                    table.setAttribute("class","table table-sm table-dark");
+
+                    var column = 2;
+                    var row = table.insertRow(-1);
+                    var headerTable = document.createElement("th");
+                    headerTable.setAttribute("colspan","3");
+                    headerTable.innerHTML = "Table "+j;
+                    row.appendChild(headerTable);
+
+                    var row = table.insertRow(-1);
+                    for (var i=0;i<column;i++){
+                        var headerCell = document.createElement("th");
+                        headerCell.innerHTML = header[i];
+                        row.appendChild(headerCell);
+                    }
+
+                    for (var i=0;i<Object.keys(orders[j].orderAmountsMap).length;i++){
+                        row = table.insertRow(-1);
+                        var cell = row.insertCell(-1);
+                        cell.innerHTML = Object.keys(orders[j].orderAmountsMap)[i];
+                        var cell = row.insertCell(-1);
+                        cell.innerHTML = orders[j].orderAmountsMap[Object.keys(orders[j].orderAmountsMap)[i]];
+                    }
+
+                    dvTable.appendChild(document.createElement("br"));
+                    dvTable.appendChild(table);
                 }
 
-                for (var i=0;i<Object.keys(orders[j].orderAmountsMap).length;i++){
-                    row = table.insertRow(-1);
-                    var cell = row.insertCell(-1);
-                    cell.innerHTML = Object.keys(orders[j].orderAmountsMap)[i];
-                    var cell = row.insertCell(-1);
-                    cell.innerHTML = orders[j].orderAmountsMap[Object.keys(orders[j].orderAmountsMap)[i]];
-                }
-
-                dvTable.appendChild(document.createElement("br"));
-                dvTable.appendChild(table);
+            },
+            onFailed: function(exception){
+                console.log(exception);
+                alert("There is a problem with our servers. We apologize for the inconvince, please try again later");
             }
-
-        },
-        onFailed: function(exception){
-            console.log(exception);
-            alert("There is a problem with our servers. We apologize for the inconvince, please try again later");
-        }
    
+        };
+    
+        RestControllerModule.getOrders(callback);
+    };
+
+    var updateOrder = function () {
+      // todo implement
+    };
+
+    var deleteOrderItem = function (itemName) {
+        var callback = {
+            onSuccess: function (orderId){
+                document.getElementById("Table"+orderId).remove();
+            },
+            onFailed: function(exception){
+                console.log(exception);
+                alert("There is a problem with our servers. We apologize for the inconvince, please try again later");
+            }
+        };
+        RestControllerModule.deleteOrder(itemName,callback);
+    };
+
+    var addItemToOrder = function (orderId, item) {
+      // todo implement
     };
     
-    RestControllerModule.getOrders(callback);
-  };
+    var loadSelectedTables = function (){
+        var callback = {
+            onSuccess: function(orders){
+                //$('#orders').append("<button id='selectTable' class='btn btn-dark dropdown-toggle' type='button' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>Select table</button>");
+                //$('#orders').append("<div class='dropdown-menu list-group-item-dark' aria-labelledby='selectTable'>");
+                $('#orders').append("<option selected>Select table</option>");
+                for (i in orders){
+                    $('#orders').append("<option value='"+i+"'>Table "+i+"</option>");
+                }
+                //$('#orders').append("</div>");
+            },
+            onFailed: function(exception){
+                console.log(exception);
+                alert("There is a problem with our servers. We apologize for the inconvince, please try again later");
+            }
+        };
+        RestControllerModule.getOrders(callback);
+    };
+    
+    var selectTable = function (){
+        
+        var index = document.getElementById("orders");
+        var sel = index.options[index.selectedIndex].value;
+        var callback = {
+            onSuccess: function (order){
+                orders = order;
+            },
+            onFailed: function(exception){
+                console.log(exception);
+                alert("There is a problem with our servers. We apologize for the inconvince, please try again later");
+            }
+        };
+    };
 
-  var updateOrder = function () {
-    // todo implement
-  };
-
-  var deleteOrderItem = function (itemName) {
-    // todo implement
-  };
-
-  var addItemToOrder = function (orderId, item) {
-    // todo implement
-  };
-
-  return {
-    showOrdersByTable: showOrdersByTable,
-    //updateOrder: updateOrder,
-    //deleteOrderItem: deleteOrderItem,
-    //addItemToOrder: addItemToOrder
+    return {
+        showOrdersByTable: showOrdersByTable,
+        updateOrder: updateOrder,
+        deleteOrderItem: deleteOrderItem,
+        addItemToOrder: addItemToOrder,
+        selectTable: selectTable,
+        loadSelectedTables: loadSelectedTables
   };
 
 })();
--- src/main/resources/static/js/RestaurantRestController.js
+++ src/main/resources/static/js/RestaurantRestController.js
@@ -1,32 +1,50 @@
 var RestControllerModule = (function () {
 
-  var getOrders = function (callback) {
-    axios.get('/orders')
-        .then(function (orders){
-            callback.onSuccess(orders.data);
-        })
-        .catch(function (error) {
-            callback.onFailed(error);
-        });
-  };
+    var getOrders = function (callback) {
+        axios.get('/orders')
+            .then(function (orders){
+                return callback.onSuccess(orders.data);
+            })
+            .catch(function (error) {
+                callback.onFailed(error);
+            });
+    };
 
-  var updateOrder = function (order, callback) {
-    // todo implement
-  };
+    var updateOrder = function (order, callback) {
+        axios.put('/orders',order)
+            .then(function(){
+                callback.onSuccess();
+            })
+            .catch(function(error){
+                callback.onFailed(error);
+            })
+    };
 
-  var deleteOrder = function (orderId, callback) {
-    // todo implement
-  };
+    var deleteOrder = function (orderId, callback) {
+        axios.delete('/orders/'+orderId)
+            .then(function(response){
+                callback.onSuccess(response);
+            })
+            .catch(function (error){
+                callback.onFailed(error);
+            })
+    };
 
-  var createOrder = function (order, callback) {
-    // todo implement
-  };
+    var createOrder = function (order, callback) {
+        axios.post('/orders',order)
+            .then(function(){
+                callback.onSuccess();
+            })
+            .catch(function(error){
+                callback.onFailed(error);
+            })
+    };
 
-  return {
-    getOrders: getOrders,
-    //updateOrder: updateOrder,
-    //deleteOrder: deleteOrder,
-    //createOrder: createOrder
-  };
+    return {
+        getOrders: getOrders,
+        updateOrder: updateOrder,
+        deleteOrder: deleteOrder,
+        createOrder: createOrder
+    };
 
 })();
